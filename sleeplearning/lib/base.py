fixed power spectral calculation with artefact exclusion, code clean up and nicer visualizations of spectrograms, qualitive analysis, psd

sleeplearning/lib/base.py
@@ -6,11 +6,13 @@ from typing import Tuple
 
 
 class SleepLearning(object):
-    """Base class which contains the data related to a sample of sleep learning
+    """Base class which contains the data related to a single day/night of of a
+    single subject. There is a classmethod for every support file format which
+    can be used to read in the file and store it as sleeplearning object. To
+    support a new file format / dataset, a new class method has to be created.
 
     Attributes
     ----------
-    id_ : string identifier of subject
 
     psgs_ : iterable of dictionaries
         Uniform raw data for various input formats and taken from one subject.
@@ -21,13 +23,12 @@ class SleepLearning(object):
 
     spectograms_: dictionary
 
-
-    prediction_ : array, shape = [numEpochs]
-        Predicted  sleep stages
     """
-    sleep_stages_labels =  {0: 'WAKE', 1: "N1", 2: 'N2', 3: 'N3', 4: 'N4', 5: 'REM', 6: 'Artifact'}
+    sleep_stages_labels = {0: 'WAKE', 1: "N1", 2: 'N2', 3: 'N3', 4: 'N4',
+                           5: 'REM', 6: 'Artifact'}
 
-    def __init__(self, id, psgs: dict, hypnogram, sampling_rate, epoch_size):
+    def __init__(self, id, psgs: dict, hypnogram, artefact_data, sampling_rate,
+                 epoch_size):
         self.id_ = id
         self.psgs_ = psgs
         self.spectograms_ = {}
@@ -35,25 +36,26 @@ class SleepLearning(object):
         self.sampling_rate_ = sampling_rate
         self.epoch_size = epoch_size
         self.window = self.sampling_rate_ * 2
-        self.stride = 250 # 50
-        self.prediction = np.array([])
+        self.stride = self.sampling_rate_
+        self.artefact_data = artefact_data
 
-    def test(self):
-        print("hi")
-    ## Input parsing functions
-
-    # Example
-    # TODO: add example
+    ## Input parsing functions for different datasets
     @classmethod
-    def _read_mat(cls, id: str, filepath: str, psg_dict: dict, hypnogram_key: str = None,
-                  epoch_size=20, sampling_rate=250):
+    def read_caro(cls, filepath: str):
         psg = {}
+        psg_dict = {'EEG': 'EEG_data_filt', 'EOGR': 'EOGR_data_filt',
+                    'EOGL': 'EOGL_data_filt', 'EMG': 'EMG_data_filt'}
+
         mat = scipy.io.loadmat(filepath)
+        artefact_data = {'artefacts': mat['artfact_per4s'][0], 'epoch_size': 4}
+        sampling_rate = int(mat['sampling_rate'][0][0])
+        epoch_size = int(mat['epoch_size_scoring_sec'][0][0])
+
         num_labels = None
 
-        if hypnogram_key is not None:
-            hypnogram = mat[hypnogram_key][0]
-            num_labels = len(mat[hypnogram_key][0])
+        if 'sleepStage_score' in mat:
+            hypnogram = mat['sleepStage_score'][0]
+            num_labels = len(mat['sleepStage_score'][0])
         else:
             hypnogram = None
 
@@ -62,7 +64,7 @@ class SleepLearning(object):
             if num_labels is None:
                 # assuming maximal possible epochs given samples
                 num_labels = num_samples // (
-                            epoch_size * sampling_rate)
+                        epoch_size * sampling_rate)
             samples_wo_label = num_samples - (num_labels *
                                               epoch_size * sampling_rate)
             print(k + ": cutting ", samples_wo_label / sampling_rate,
@@ -71,36 +73,84 @@ class SleepLearning(object):
             # eeg_cut = eeg_cut.reshape(num_labels, -1)
             psg[k] = eeg_cut
 
+        subject_label = filepath.split('/')[-1][5:-12]
+        return cls(subject_label, psg, hypnogram, artefact_data, sampling_rate,
+                   epoch_size)
 
-        return cls(id, psg, hypnogram, sampling_rate, epoch_size)
+    @classmethod
+    def _read_physionet_npz(cls, id: str, filepath: str):
+        psg = {}
+        with np.load(filepath) as f:
+            psg['EEG'] = f["x"].reshape(-1)
+            labels = f["y"]
+            labels[labels == 5] = 6  # UNK -> ARTEFACT
+            labels[labels == 4] = 5  # REM is now 5
+            sampling_rate = int(f["fs"])
+        return cls(id, psg, labels, None, sampling_rate, epoch_size=30)
 
-    def get_spectograms(self, psg_key: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        if psg_key in self.spectograms_:
-            return self.spectograms_[psg_key]
+    def get_spectrograms(self, channel: str) -> Tuple[
+        np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Compute the spectogram for a specific channel and for every epoch and
+        return a tuple of (frequencies,times,[spectrograms]) where spectrograms
+        is a numpy array containing a spectrogram for every epoch
+        :param channel: channel key as stored in self.psgs_
+        :return: frequencies [fs/2+1], times [epoch size*fs/stride],
+        spectrogram (magnitudes) [total epochs, fs/2+1, epoch size*fs/stride ]
+        """
+        if channel in self.spectograms_:
+            return self.spectograms_[channel]
         f = t = 0
         Sxxs = []
+        artefacts = np.repeat(self.artefact_data['artefacts'],
+                              self.artefact_data[
+                                  'epoch_size'] * self.sampling_rate_)
         # reshape to [num epochs, samples per epoch]
-        psgs = self.psgs_[psg_key].reshape((-1, self.sampling_rate_ * self.epoch_size))
+        psgs = self.psgs_[channel].reshape(
+            (-1, self.sampling_rate_ * self.epoch_size))
+        artefacts = artefacts.reshape(
+            (-1, self.sampling_rate_ * self.epoch_size))
         padding = self.window // 2 - self.stride // 2
         psgs = np.pad(psgs, pad_width=((0, 0), (padding, padding)), mode='edge')
-        for psg in psgs:
-            f, t, Sxx = signal.spectrogram(psg, fs=self.sampling_rate_, nperseg=self.window,
+        artefacts = np.pad(artefacts, pad_width=((0, 0), (padding, padding)),
+                           mode='edge')
+        for psg, artefact in zip(psgs, artefacts):
+            psg_clean = psg  # psg[artefact == 0]
+            f, t, Sxx = signal.spectrogram(psg_clean, fs=self.sampling_rate_,
+                                           nperseg=self.window,
                                            noverlap=self.window - self.stride,
                                            scaling='density', mode='magnitude')
             Sxxs.append(Sxx)
-        self.spectograms_[psg_key] = (f, t, np.array(Sxxs))
-        return self.spectograms_[psg_key]
+        self.spectograms_[channel] = (f, t, np.array(Sxxs))
+        return self.spectograms_[channel]
 
-    def get_periodograms(self, psg_key: str) -> Tuple[np.ndarray, np.ndarray]:
+    def get_psds(self, channel: str) -> Tuple[np.ndarray, np.ndarray]:
         """
+        Compute the power spectral densities for a specific channel and for
+        every epoch.
+        :param channel: channel key as stored in self.psgs_
+        :return: frequencies [fs/2+1], psds [numEpochs, fs/2+1]
+        """
+        pxxs = []
+        artefacts = np.repeat(self.artefact_data['artefacts'],
+                              self.artefact_data[
+                                  'epoch_size'] * self.sampling_rate_)
+        # reshape to [num epochs, samples per epoch]
+        psgs = self.psgs_[channel].reshape(
+            (-1, self.sampling_rate_ * self.epoch_size))
+        artefacts = artefacts.reshape(
+            (-1, self.sampling_rate_ * self.epoch_size))
 
-        :param psg_key:
-        :return:
-        """
-        f, t, Sxxs = self.get_spectograms(psg_key)
-        psds = []
-        for Sxx in Sxxs:
-            psd = (Sxx / 2) ** 2 / self.window
-            psd = np.sum(psd, axis=1)
-            psds.append(psd)
-        return f, np.array(psds)
+        padding = self.window // 2 - self.stride // 2
+        psgs = np.pad(psgs, pad_width=((0, 0), (padding, padding)), mode='edge')
+        artefacts = np.pad(artefacts, pad_width=((0, 0), (padding, padding)),
+                           mode='edge')
+        f = 0
+        for psg, artefact in zip(psgs, artefacts):
+            psg_clean = psg[artefact == 0]
+            f, pxx = signal.welch(psg_clean, fs=self.sampling_rate_,
+                                  nperseg=self.window,
+                                  noverlap=self.window - self.stride,
+                                  scaling='density')
+            pxxs.append(pxx)
+        return f, np.array(pxxs)

sleeplearning/lib/visualize.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 
+
 class Visualize(object):
-    "Class to visualize psg data of sleeplearning objects"
+    "Class to visualize psg data of a list of sleeplearning objects"
+
     def __init__(self, sleeplearnings: List[SleepLearning]):
         self.data = sleeplearnings
 
@@ -19,18 +21,20 @@ class Visualize(object):
             subject_labels.append(sl.id_)
         plt.figure(figsize=(10, 5))
 
-        plt.hist(sleep_stage_dist, label=subject_labels, bins=np.arange(8) - 0.5)
+        plt.hist(sleep_stage_dist, label=subject_labels,
+                 bins=np.arange(8) - 0.5)
         plt.legend()
         plt.ylabel('count')
         plt.title(
             "Sleep Phases Distribution ({0} labels)".format(str(total_labels)))
-        _ = plt.xticks(np.arange(7), list(SleepLearning.sleep_stages_labels.values()))
+        _ = plt.xticks(np.arange(7),
+                       list(SleepLearning.sleep_stages_labels.values()))
 
     def transition_distribution(self):
         num_sleep_phases = len(SleepLearning.sleep_stages_labels.keys())
         M = np.zeros((num_sleep_phases, num_sleep_phases))
         for sl in self.data:
-            for i, j in zip(sl.hypnogram,sl.hypnogram[1:]):
+            for i, j in zip(sl.hypnogram, sl.hypnogram[1:]):
                 M[i, j] += 1
         cmap = plt.cm.Blues
         M /= (0.0001 + np.sum(M, axis=1)[:, np.newaxis])
@@ -45,91 +49,112 @@ class Visualize(object):
                      horizontalalignment="center",
                      color="black")
 
-    def periodogram(self, psg_key: str, sleep_phases : List[int]):
-        plt.figure()
+    def psd(self, channel: str, sleep_phases: List[int]):
+        plt.figure(figsize=(10, 5))
         ax = plt.subplot('111')
         for i in sleep_phases:
             y = np.array([])
             for sl in self.data:
-                f, periodograms = sl.get_periodograms(psg_key)
+                f, periodograms = sl.get_psds(channel)
                 ind = np.where(sl.hypnogram == i)[0]
                 y_add = periodograms[ind]
+                # remove none types (epochs with only artefacts)
+                y_add = y_add[np.array([y.size > 0 for y in y_add])]
                 y = np.vstack([y, y_add]) if y.size else y_add
             y_mean = np.mean(y, axis=0)
             error = np.std(y, axis=0)
 
             ax.plot(f, y_mean, label=SleepLearning.sleep_stages_labels[i])
-            ax.fill_between(f, y_mean - error / 5, y_mean + error / 5,
-                             alpha=0.3)
+            # ax.fill_between(f, y_mean - error/2, y_mean + error/2,
+            #                 alpha=0.3)
 
-        ax.set_xlim(xmin=0.5, xmax=24)
+        ax.set_xlim(xmin=0.5, xmax=50)
         ax.set_ylim(ymin=0.0099)
         ax.set_xlabel("Frequency (Hz)")
-        ax.set_ylabel("PSD [mV**2/Hz]")
+        ax.set_ylabel("PSD [$\mu$V**2/Hz]")
+        ax.set_title("Mean Power Spectral Density ")
         ax.set_yscale("log")
         ax.legend()
 
-    def epoch(self, index : int, psg_key : str):
+    def epoch(self, epoch_index: int, channel: str):
         for sl in self.data:
             if sl.hypnogram is not None:
-                sleep_phase = sl.hypnogram[index]
+                sleep_phase = sl.hypnogram[epoch_index]
                 sleep_phase = SleepLearning.sleep_stages_labels[sleep_phase]
             else:
                 sleep_phase = "UNK"
             f, axarr = plt.subplots(1, 3, sharex=False, figsize=(20, 5))
-            f.suptitle('['+sl.id_+'] - epoch '+ str(index) + ' - sleep phase: ['+sleep_phase +']', fontsize=16)
+            f.suptitle('[' + sl.id_ + '] - epoch ' + str(
+                epoch_index) + ' - sleep phase: [' + sleep_phase + ']',
+                       fontsize=16)
             t = 20 * np.arange(5000, dtype=float) / 5000
             # t+=offset/30
-            samples_per_epoch = sl.sampling_rate_*sl.epoch_size
-            psg = sl.psgs_[psg_key][index*samples_per_epoch:(index+1)*samples_per_epoch]
+            samples_per_epoch = sl.sampling_rate_ * sl.epoch_size
+            psg = sl.psgs_[channel][epoch_index * samples_per_epoch:
+                                    (epoch_index + 1) * samples_per_epoch]
             axarr[0].plot(t, psg)
             axarr[0].set_xlabel("time [s]")
             axarr[0].set_ylabel("mV")
 
-
-            f, t, Sxx_list = sl.get_spectograms(psg_key)
-            Sxx = Sxx_list[index]
-            # print("Sxx: ", Sxx.shape)
-            # print("f shape: ", f.shape)
-            Sxx_cut = Sxx#Sxx[(f <= 38)]
-            f_cut = f#f[(f <= 38)]
-            axarr[1].pcolormesh(t, f_cut, Sxx_cut)
+            f, t, Sxx_list = sl.get_spectrograms(channel)
+            Sxx = Sxx_list[epoch_index]
+            Sxx = Sxx ** 2  # psd from magnitude spectrum
+            db = 10 * np.log10(Sxx)  # convert to dB
+            axarr[1].pcolormesh(t, f, db, cmap='jet', vmin=-15)
             axarr[1].invert_yaxis()
             axarr[1].set_xlabel("time [s]")
-            axarr[1].set_ylabel(psg_key+ ' spectogram [Hz]')
+            axarr[1].set_ylabel('frequency [Hz]')
             axarr[1].set_ylim(ymin=30)
-            f, Pxx_list = sl.get_periodograms(psg_key)
-            pxx = Pxx_list[index]
+            f, Pxx_list = sl.get_psds(channel)
+            pxx = Pxx_list[epoch_index]
             axarr[2].plot(f, pxx)
-            axarr[2].set_ylabel("mV**2")
+            axarr[2].set_ylabel("$\mu$V**2/Hz")
+            axarr[2].set_xlabel("frequency (Hz)")
+            axarr[2].set_xlim(xmax=50)
+            axarr[2].set_yscale('log')
 
-    def qualitative_analysis(self, psg_key, from_epoch = 0, to_epoch = None):
+    def qualitative_analysis(self, channel, from_epoch=0, to_epoch=None):
         for sl in self.data:
             if to_epoch is None:
                 # set to last epoch of sample if no value given
-                to_epoch = len(sl.psgs_[psg_key])//sl.epoch_size//sl.sampling_rate_
+                to_epoch = int(len(
+                    sl.psgs_[channel]) // sl.epoch_size // sl.sampling_rate_)
 
-            fig, axarr = plt.subplots(3, sharex=True, sharey=False, figsize=(20, 20))
-
+            fig, axarr = plt.subplots(3, sharex=True, sharey=False,
+                                      figsize=(20, 20))
+            fig.suptitle(
+                '[' + sl.id_ + '] Qualitative Analysis (epoch length ' + str(
+                    sl.epoch_size) + 's)', fontsize=16)
             samples_per_epoch = sl.sampling_rate_ * sl.epoch_size
+            print(samples_per_epoch)
             axarr[0].set_xlim(xmin=from_epoch, xmax=to_epoch)
-            axarr[0].plot(np.linspace(from_epoch, to_epoch, (to_epoch-from_epoch)*samples_per_epoch), sl.psgs_['FPZ'][from_epoch*samples_per_epoch:to_epoch*samples_per_epoch])
+            axarr[0].plot(np.linspace(from_epoch, to_epoch, (
+                        to_epoch - from_epoch) * samples_per_epoch),
+                          sl.psgs_[channel][from_epoch * samples_per_epoch:
+                                            to_epoch * samples_per_epoch])
             axarr[0].set_ylim(ymin=-300, ymax=300)
             axarr[0].set_ylabel('mV')
-            f, t, Sxx_list = sl.get_spectograms(psg_key)
+            f, t, Sxx_list = sl.get_spectrograms(channel)
             Sxx = Sxx_list[from_epoch:to_epoch]
-            Sxx_cut = Sxx  # Sxx[(f >= 40)]
-            Sxx_avg = np.mean(Sxx, axis=2)
-            Sxx_avg = Sxx_avg.T
-
-            axarr[1].pcolormesh(range(from_epoch, to_epoch), f, Sxx_avg, vmin=0, vmax=20,
-                                 cmap='jet')
-            axarr[1].set_ylim(ymax=18)
+            # compute psd from magnitude
+            Sxx = Sxx ** 2
+            dBS = 10 * np.log10(Sxx)  # convert to dB
+            dBS = dBS.swapaxes(0, 1)
+            dBS = dBS.reshape((dBS.shape[0], -1))
+            pc = axarr[1].pcolormesh(np.linspace(from_epoch, to_epoch,
+                len(t) * (to_epoch - from_epoch)),f, dBS, cmap='jet', vmin=-15)
+            axarr[1].set_ylim(ymax=40)
             axarr[1].invert_yaxis()
             axarr[1].set_ylabel("spectogram [Hz]")
-            h = range(from_epoch, to_epoch+1)
-            axarr[2].plot(h, sl.hypnogram[from_epoch:to_epoch+1])
-            axarr[2].set_yticks(range(0,len(SleepLearning.sleep_stages_labels.keys())))
+            h = range(from_epoch, to_epoch)
+            axarr[2].plot(h, sl.hypnogram[from_epoch:to_epoch])
+            axarr[2].set_yticks(
+                range(0, len(SleepLearning.sleep_stages_labels.keys())))
             axarr[2].set_yticklabels(SleepLearning.sleep_stages_labels.values())
             axarr[2].set_xlim(xmin=from_epoch, xmax=to_epoch)
             axarr[2].set_xlabel("epoch")
+            position = fig.add_axes([0.74, 0.633, 0.15, 0.01])
+            cbar = fig.colorbar(pc, cax=position, orientation='horizontal')
+            cbar.ax.set_title('Power (dB)')
+            # cbar.set_ticks([zmin, (zmax+zmin)/2, zmax])
+            # cbar.set_ticklabels([zmin, (zmax+zmin)/2, zmax])