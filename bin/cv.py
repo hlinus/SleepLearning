ffbf9924a833c590b9ad843996705285d598fb82
hlinus <hlinus@ethz.ch>
2018-08-15 15:12:20 +0200

    new file based cross validation

--- bin/cv.py
+++ bin/cv.py
@@ -1,11 +1,15 @@
 import argparse
-import glob
 import os
+import shutil
+
+import pandas as pd
 import sys
 
+from sacred.stflow import LogFileWriter
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-from sleeplearning.lib.logger import cm_figure_
+from sleeplearning.lib.base import Base
+from sleeplearning.lib.logger import cm_figure_, Logger
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname('__file__'), '..'))
 sys.path.insert(0, root_dir)
@@ -14,52 +18,58 @@ import numpy as np
 parser = argparse.ArgumentParser(prog='Sleep Learning cross validation')
 parser.add_argument("--data_dir", dest="fn", default=os.path.abspath(
     os.path.join(os.path.dirname(__file__),
-                 '..')) + '/logs/ZZ_test5',
+                 '..')) + '/logs/ ',
                         help="filepath of cv folds")
 
-
-def evaluate(files):
-    accuracies = np.array([])
-    targets = np.array([], dtype=int)
-    predictions = np.array([], dtype=int)
-    for f in files:
-        results = np.load(f)
-        tar = results['targets'].astype('int')
-        pred = results['predictions'].astype('int')
-        epoch = results['epoch'].astype('int')
-        acc_f = results['acc']
-        acc = accuracy_score(tar, pred)
-        print("fold: ",f , " epoch: ", epoch, " acc:",acc, "accf:", acc_f)
-        accuracies = np.append(accuracies, acc)
-        targets = np.append(targets, tar)
-        predictions = np.append(predictions, pred)
-
-    cm = confusion_matrix(targets, predictions)
-    print("confusion matrix: ")
-    print(cm)
-
-    mean_acc = np.mean(accuracies)
-    std_acc = np.std(accuracies)
-    print("\n\naccuracies: ", accuracies)
-    print("mean: ", mean_acc, "std: ", std_acc, "\n\n")
-    fig = cm_figure_(predictions, targets, ["W", "N1", "N2", "N3", "REM"])
-    return mean_acc, fig
+def get_basename_(path):
+    return os.path.basename(os.path.normpath(path))
 
 @ex.main
 def cv(ds, arch, ms, cuda, log_dir, seed, _run):
-    print("\n\n TRAIN: \n\n")
-    train_files = glob.glob(os.path.join(log_dir, '**',
-                                               '*train*last.npz'),
-                          recursive=True)
+    cuda = torch.cuda.is_available()
 
-    _, f_train = evaluate(train_files)
-    f_train.savefig('train_cm.png')
+    output_dir = os.path.join(log_dir, get_basename_(ds['val_csv']))
+    if os.path.exists(output_dir) and os.path.isdir(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
 
-    print("\n\n VALIDATION: \n\n")
-    val_files = glob.glob(os.path.join(log_dir, '**', '*val*last.npz'), recursive=True)
-    mean_val_acc, f_val = evaluate(val_files)
-    f_val.savefig('val_cm.png')
-    return mean_val_acc*100
+    if _run._id is not None:
+        # Capture TensorBoard logs with sacred
+        with LogFileWriter(ex):
+            logger = Logger(log_dir, _run)
+    else:
+        logger = Logger(log_dir, None)
+
+    targets = np.array([], dtype=int)
+    predictions = np.array([], dtype=int)
+    clf = Base(logger=logger, cuda=cuda, verbose=True)
+
+    for i in range(20):
+        subjects = pd.read_csv(ds['val_csv'], header=None)[i].dropna().tolist()
+        for subject in subjects:
+            checkpoint_path = os.path.join(log_dir, f'fold{i}', 'checkpoint.pth.tar')
+            clf.restore(checkpoint_dir=checkpoint_path)
+            # TODO: fix hack for correct csv load
+            clf.ds['fold'] = 0
+            acc, probs, true = clf.score(os.path.join(ds['data_dir'], subject),
+                              probs=True)
+            pred = np.argmax(probs, 1)
+            savedict = dict(subject=subject, acc=acc, probs=probs, y_pred=pred,
+                            y_true=true)
+
+
+            np.savez(os.path.join(output_dir, subject), **savedict)
+            targets = np.append(targets, true)
+            predictions = np.append(predictions, pred)
+
+    cm = confusion_matrix(targets, predictions)
+    overall_acc = accuracy_score(targets, predictions)*100
+    print("\n CONFUSION MATRIX ")
+    print(cm)
+    print("\n OVERALL ACCURACY ")
+    print(f"{overall_acc:.2f}%")
+
+    return overall_acc
 
 
 if __name__ == '__main__':
