bd5af17b05abecfe171c7fdd8230718d7dd97fc7
hlinus <hlinus@ethz.ch>
2018-10-23 16:59:43 +0200

    init file for used models

--- sleeplearning/lib/models/__init__.py
+++ sleeplearning/lib/models/__init__.py
@@ -1,2 +1,4 @@
-__all__ = ["deep_sleep_net", "sleep_stage", "multivariate_net",
-           "multivariate_net2d", "multivariate2d_net", "experts_avg"]
+__all__ = ["single_chan_expert",
+           "late_fusion",
+           "amoe",
+           "attention_net"]
